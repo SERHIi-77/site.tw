2e8e386ce25ce05375d5c05eb4c05d595d8dd9d2
SVS <svskorokhod@gmail.com>
2023-03-17 12:42:57 +0200

    edit add_ad

--- partials/ads/add_ad.php
+++ partials/ads/add_ad.php
@@ -20,9 +20,9 @@ if(!empty($_FILES) && $_FILES['photo']['error'] != 4) {
     }
 }
 if (isset($_POST['submit'])) {
-	var_dump($_POST);
-	$sql = "INSERT INTO pets (title, ad, type, sex, age, breed, owner, price, photo, note) 
-	VALUES ('" . $_POST["title"] . "', '" . $_POST["ad"] . "', '" . $_POST["type"] . "', '" . $_POST["sex"] . "', '" . $_POST["age"] . "', '" . $_POST["breed"] . "', '" . $_POST["owner"] . "', '" . $_POST["price"] . "', '" . $photoName . "', '" . $_POST["note"] . "' )";
+	//var_dump($_POST);
+
+	$sql = "INSERT INTO pets (title, ad, type, sex, age, breed, owner, price, photo, note) VALUES ('" . $_POST["title"] . "', '" . $_POST["ad"] . "', '" . $_POST["type"] . "', '" . $_POST["sex"] . "', '" . $_POST["age"] . "', '" . $_POST["breed"] . "', '" . $_POST["owner"] . "', '" . $_POST["price"] . "', '" . $photoName . "', '" . $_POST["note"] . "' )";
 	// INSERT INTO `pets` (`id`, `title`, `ad`, `type`, `sex`, `age`, `breed`, `owner`, `price`, `photo`, `note`, `created`) VALUES (NULL, '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', current_timestamp());
 
 	if (mysqli_query($conn, $sql)) {
@@ -55,33 +55,32 @@ if (isset($_POST['submit'])) {
                 </div>
 				<div class="form-group-row">
                     <label for="ad_type">type</label>
-                    <input class="form-control rounded" list="datalist_type" name="type" id="ad_type"></input>
-					<datalist id="datalist_type">
-						<option value="dog">
-						<option value="cat">
-					</datalist>
+                    <select class="form-select rounded" name="type" id="ad_type">
+                        <option selected></option>
+                        <option value="dog">dog</option>
+						<option value="cat">cat</option>
+                    </select>
                 </div>
 				<div class="form-group-row">
                     <label for="ad_sex">sex</label>
-                    <input class="form-control rounded" list="datalist_sex" name="sex" id="ad_sex"></input>
-					<datalist id="datalist_sex">
-						<option value="male">
-						<option value="female">
-					</datalist>
-                </div>
+                    <select class="form-select rounded" name="sex" id="ad_sex">
+                        <option selected></option>
+                        <option value="male">male</option>
+						<option value="female">female</option>
+                    </select>
 				<div class="form-group-row">
                     <label for="ad_age">age</label>
-                    <input class="form-control rounded" list="datalist_age" name="age" id="ad_age"></input>
-					<datalist id="datalist_age">
-						<option value="less than 3 months">
-						<option value="3-6 months">
-						<option value="6-9 months">
-						<option value="9-12 months">
-						<option value="1-2 years">
-						<option value="3 years">
-						<option value="4 years">
-						<option value="over 5 years">
-					</datalist>
+                    <select class="form-select rounded" name="age" id="ad_age">
+                        <option selected></option>
+                        <option value="less than 3 months">less than 3 months</option>
+						<option value="3-6 months">3-6 months</option>
+						<option value="6-9 months">6-9 months</option>
+						<option value="9-12 months">9-12 months</option>
+						<option value="1-2 years">1-2 years</option>
+						<option value="3 years">3 years</option>
+						<option value="4 years">4 years</option>
+						<option value="over 5 years">over 5 years</option>
+                    </select>
                 </div>
 				<div class="form-group-row">
                     <label for="ad_breed">breed</label>
