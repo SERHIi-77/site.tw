496d970e24db765da0197f8958e9317324bfe028
SVS <svskorokhod@gmail.com>
2023-03-19 21:19:34 +0200

    editing card ads

--- admin/partials/pages/ads.php
+++ admin/partials/pages/ads.php
@@ -5,38 +5,37 @@ $ads = getAllAdsByUser(getUserID());
 
 <?php while($row = $ads->fetch_assoc()): ?>
 <!-- Card with an image on left -->
-<div class="card mb-3">
-    <div class="row g-0">
-        <div class="col-md-4">
-            <img src="/uploads/<?php echo($row['photo']);?>" class="img-fluid rounded-start" alt="...">
+<div class="card">
+    <div class="card-header d-flex justify-content-between">
+        <div >
+            Header
         </div>
-        <div class="col-md-8">
-            <div class="card-body">
-                <h5 class="card-title"><?php echo $row['title']; ?></h5>
-                <p class="card-text"><?php echo $row['ad']; ?></p>
+        <div>
+            <button class="btn btn-danger rounded-pill"><i class="bi bi-trash3"></i> Видалити</button>
+        </div>
+            
+   </div>
+
+    <div class="card mb-3">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="/uploads/<?php echo($row['photo']);?>" class="img-fluid rounded-start" alt="..." style="height: 300px;">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $row['title']; ?></h5>
+                    <p class="card-text"><?php echo $row['ad']; ?></p>
+                </div>
             </div>
         </div>
     </div>
+    <div class="card-footer d-flex justify-content-between">
+        Footer
+        <button class="btn btn-success rounded-pill"><i class="bi bi-pencil-square"></i> Редагування</button>
+    </div>
 </div>
-<?php endwhile; ?>
+
+
+
 <!-- End Card with an image on left -->
-
-
-
-
-
-<!-- <div class="container" >
-    <ul id="listTwits" class="list-group">
-        <?php while($row = $twits->fetch_assoc()): ?>
-            <li class="list-group-item">
-                <?php echo $row['twit']; 
-                if ($row['image'] != ""): ?>
-                    <div class="d-flex justify-content-end">
-                        <img class="img-fluid img-rounded float-right" width="100" src="/uploads/<?php echo $row['image']; ?>">
-                    </div>
-                <?php endif; ?>
-        
-            </li>
-        <?php endwhile; ?>
-    </ul>
-</div> -->
+<?php endwhile; ?>
