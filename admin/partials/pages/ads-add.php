9bed77b19ad571130eebc5a0bbc7853023120b1c
SVS <svskorokhod@gmail.com>
2023-03-21 00:03:23 +0200

    fix form

--- admin/partials/pages/ads-add.php
+++ admin/partials/pages/ads-add.php
@@ -31,7 +31,7 @@ if (isset($_POST['submit'])) {
 
 	if (mysqli_query($conn, $sql)) {
 
-		header("Location: /?p=catalog");
+		echo('<script> window.location.href = "/admin/index.php?p=ads"; </script>');;
 
 	} else {
 		echo "Error:" . $sql . "<br>" . mysqli_error($conn);
@@ -41,70 +41,77 @@ if (isset($_POST['submit'])) {
 
 ?>
 
-<h2>Додати оголошення</h2>
-<div class="container">
-    <div class="card">
-        <div class="card-body">
-            <!-- <div id="ajaxStatus"></div> -->
-            <form class="form-group" action="#" method="POST" id="formAd" form enctype="multipart/form-data">
-                <div class="form-group">
-                    <input type="hidden" name="owner" value="<?php echo getUserID(); ?>">
-				<div class="form-group-row">
-                    <label for="ad_title">Title Ad</label>
-                    <textarea class="form-control rounded" name="title" id="ad_title" rows="1"></textarea>
-                </div>
-				<div class="form-group-row">
-                    <label for="ad_text">Your Ad</label>
-                    <textarea class="form-control rounded" name="ad" id="ad_text" rows="5"></textarea>
-                </div>
-				<div class="form-group-row">
-                    <label for="ad_type">type</label>
-                    <select class="form-select rounded" name="type" id="ad_type">
-                        <option selected></option>
-                        <option value="dog">dog</option>
-						<option value="cat">cat</option>
-                    </select>
-                </div>
-				<div class="form-group-row">
-                    <label for="ad_sex">sex</label>
-                    <select class="form-select rounded" name="sex" id="ad_sex">
-                        <option selected></option>
-                        <option value="male">male</option>
-						<option value="female">female</option>
-                    </select>
-				<div class="form-group-row">
-                    <label for="ad_age">age</label>
-                    <select class="form-select rounded" name="age" id="ad_age">
-                        <option selected></option>
-                        <option value="less than 3 months">less than 3 months</option>
-						<option value="3-6 months">3-6 months</option>
-						<option value="6-9 months">6-9 months</option>
-						<option value="9-12 months">9-12 months</option>
-						<option value="1-2 years">1-2 years</option>
-						<option value="3 years">3 years</option>
-						<option value="4 years">4 years</option>
-						<option value="over 5 years">over 5 years</option>
-                    </select>
-                </div>
-				<div class="form-group-row">
-                    <label for="ad_breed">breed</label>
-                    <input class="form-control rounded" name="breed" id="ad_breed"></input>
-                </div>
-				<div class="form-group-row">
-                    <label for="ad_price">price</label>
-                    <input class="form-control rounded" name="price" id="ad_price"></input>
-                </div>
-				<div class="form-group-row">
-                    <label for="ad_note">Your Notes</label>
-                    <textarea class="form-control rounded" name="note" id="ad_note" rows="3"></textarea>
+<div class="my-3 card">
+    <h3 class="card-title text-center"  style="font-size: 1.5rem">Додати оголошення</h3>
+    <div class="card-body">
+        <!-- <div id="ajaxStatus"></div> -->
+        <form class="form-group" action="#" method="POST" id="formAd" form enctype="multipart/form-data">
+            <div class="form-group">
+                <input type="hidden" name="owner" value="<?php echo getUserID(); ?>">
+                <div class="form-group-row">
+                    <label for="ad_title">Заголовок оголошення</label>
+                    <textarea class="form-control rounded" name="title" id="ad_title" rows="1" required="true"></textarea>
                 </div>
                 <div class="form-group-row">
-                    <i class="ti-image btn-icon-prepend"></i>   
-                    <input id="twitImage" type="file" name="photo" class="file-upload-default" ></input>    
-                    <button type="submit" name="submit" class="btn btn-primary mr-2">SEND</button>
+                    <label for="ad_text">Ваше оголошення</label>
+                    <textarea class="form-control rounded" name="ad" id="ad_text" rows="5"required="true"></textarea>
                 </div>
-			</form>
-        </div>
+                <div class="form-group-row">
+                    <label for="ad_type">Вид</label>
+                    <select class="form-select rounded" name="type" id="ad_type">
+                        <option selected></option>
+                        <option value="dog">собаки</option>
+                        <option value="cat">коти</option>
+                    </select>
+                </div>
+                <div class="form-group-row">
+                    <label for="ad_sex">Стать</label>
+                    <select class="form-select rounded" name="sex" id="ad_sex">
+                        <option selected></option>
+                        <option value="male">чоловіча стать</option>
+                        <option value="female">жіноча стать</option>
+                    </select>
+                    <div class="form-group-row">
+                        <label for="ad_age">Вік</label>
+                        <select class="form-select rounded" name="age" id="ad_age">
+                            <option selected></option>
+                            <option value="less than 3 months">менше 3 місяців</option>
+                            <option value="3-6 months">3-6 місяців</option>
+                            <option value="6-9 months">6-9 місяців</option>
+                            <option value="9-12 months">9-12 місяців</option>
+                            <option value="1-2 years">1-2 років</option>
+                            <option value="3 years">3 років</option>
+                            <option value="4 years">4 років</option>
+                            <option value="over 5 years">більш ніж 5 років</option>
+                        </select>
+                    </div>
+                    <div class="form-group-row">
+                        <label for="ad_breed">Порода</label>
+                        <input class="form-control rounded" name="breed" id="ad_breed"></input>
+                    </div>
+                    <div class="form-group-row">
+                        <label for="ad_price">Ціна</label>
+                        <input class="form-control rounded" name="price" id="ad_price"></input>
+                    </div>
+                    <div class="mb-3 form-group-row">
+                        <label for="ad_note">Ваші нотатки</label>
+                        <textarea class="form-control rounded" name="note" id="ad_note" rows="3"></textarea>
+                    </div>
+                    <div class="form-group-row">
+                        <div class="mb-3 row" >
+                            <div >
+                                <input class="form-control" type="file" name="photo" id="formFile">
+                            </div>
+                        </div>
+                        <div class="mb-3 row">
+                            <div class="text-center" >
+                                <button type="submit" name="submit" class="btn btn-primary">Додати оголошення</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
     </div>
 </div>
 
