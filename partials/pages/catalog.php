f1ca0f5a6b1b0c7afeaa5069ee9f8ba5365d604a
SVS <svskorokhod@gmail.com>
2023-03-22 16:26:12 +0200

    fixing catalog

--- partials/pages/catalog.php
+++ partials/pages/catalog.php
@@ -72,29 +72,41 @@ $sql = "SELECT * FROM pets ORDER BY created DESC";
 $result = $conn->query($sql);
 
 if($result = $conn->query($sql)):
-    foreach($result as $row):
+    foreach($result as $pet):
+
+        $favSQL = 'SELECT COUNT(*) as count FROM fav WHERE pet =' . $pet['id'];
+        // получаем общее количество лайков
+        $countFav = mysqli_fetch_assoc(mysqli_query($conn, $favSQL))['count'];
+        
+        // if(isset($_COOKIE['user'])) {
+        //     $likeUserSQL = 'SELECT count (*) as total FROM user_post_likes WHERE post_id=' . $row['id'] . 'user_id=' . $_COOKIE['user'];
+        //     // непонятная часть кода ???
+        //     $likeUserResult = $conn->query($likesSQL);
+        // }
+
+
 
 ?>
             <!-- стандартная карточка товара/ аналог карточки статьи в блоге в 13 уроке- подтянет данные из бд -->
             <div class="product-card">
-                <div class="foto-of-pet"><img src="/uploads/<?php echo($row['photo']);?>" alt=""></div>
-                <span class="title-pet"><i><p> <?php echo ($row['title']);?></p></i></span>
-                <p class="histopy-of-pet"><?php echo ($row['ad']);?></p>
+                <div class="foto-of-pet"><img src="/uploads/<?php echo($pet['photo']);?>" alt=""></div>
+                <span class="title-pet"><i><p> <?php echo ($pet['title']);?></p></i></span>
+                <p class="histopy-of-pet"><?php echo ($pet['ad']);?></p>
                 <div class="one-str-type-card">
-                    <p class="type-pet"><?php echo ($row['type']);?></p>
-                    <p class="sex-type-pet"><?php echo ($row['sex']);?></p>
-                    <p class="age-of-pet"><?php echo ($row['age']);?></p>
+                    <p class="type-pet"><?php echo ($pet['type']);?></p>
+                    <p class="sex-type-pet"><?php echo ($pet['sex']);?></p>
+                    <p class="age-of-pet"><?php echo ($pet['age']);?></p>
                 </div >
                 <div class="price-and-buy">
-                    <strong><h4 class="price-pet"><?php echo ($row['price']);?></h4></strong>
-                    <button class="i-want-by-buy" id="send1" data-pet="<?php echo $row['id']; ?>" >купити</button >
+                    <strong><h4 class="price-pet"><?php echo ($pet['price']);?></h4></strong>
+                    <button class="i-want-by-buy" id="send1" data-pet="<?php echo $pet['id']; ?>" >купити</button >
                 </div>
                 <div class="card-product-down">
 
-                    <span class="like-hearts favorite" data-pet="<?php echo $row['id']; ?>">" ></span>
-                    
+                    <span id="heartBtn" class="like-hearts favorite" data-pet="<?php echo $pet['id']; ?>"></span>
+                    <span><?php echo $countFav ?></span>
 
-                    <i><p><?php echo ($row['created']);?></p></i>
+                    <i><p><?php echo ($pet['created']);?></p></i>
                     
                 </div>
             </div>
