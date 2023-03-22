9ee0d0d560c799c601f9396ed21e927d67d8a2ed
SVS <svskorokhod@gmail.com>
2023-03-22 13:39:21 +0200

    add avatar to ligin menu

--- admin/partials/topbar.php
+++ admin/partials/topbar.php
@@ -7,7 +7,7 @@
 <div class="d-flex align-items-center justify-content-between">
   <a href="/admin/index.php" class="logo d-flex align-items-center">
     <img src="assets/img/logo.png" alt="">
-    <span class="d-none d-lg-block">Personal page</span>
+    <span class="d-none d-lg-block">Personal Cabinet</span>
   </a>
   <i class="bi bi-list toggle-sidebar-btn"></i>
 </div>
--- partials/navbar.php
+++ partials/navbar.php
@@ -1,17 +1,18 @@
-
+<?php
+$user = getCurrentUser();
+?>
 
 <div class="menuBurgerUser"> 
   <img src="../assets/img/header/user.svg">
 </div>
 
 <div class="container-user-burger">
-<div class="user-avatar" style="background-image: url('/uploads/avatars/avatarK.svg')"></div>
+<div class="user-avatar" style="background-image: url('/uploads/avatars/<?php echo $user['avatar'] ?>')"></div>
 <nav class="nav justify-content-end">
-<?php
 
+<?php
 // проверяем сесия или куки
 if (isAuth ()){
-    $user = getCurrentUser();
 ?>
   <span class="nav-item">
     <h4> Привіт, <br> <?php echo $user['username'] ?></h4><p> <br>ти можешь подати об'яву чи залишити заявку </p> 
